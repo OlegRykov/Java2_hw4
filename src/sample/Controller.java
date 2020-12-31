c032a6780d67b30599c7254e8cfd04e7adbe361b
Oleg <olegrikov@ya.ru>
2020-12-31 15:36:50 +0300

    hw4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,1 +1,27 @@
+package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class Controller {
+    @FXML
+    public TextField textField;
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    public void btnSend() {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+
+    public void enter(KeyEvent keyEvent) {
+        if (KeyCode.ENTER == keyEvent.getCode()){
+            btnSend();
+        }
+    }
+}
